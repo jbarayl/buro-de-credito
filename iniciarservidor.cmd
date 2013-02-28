088f78b4d1d62e5561cb15bee3eaa099ba087b87
Jesús Herrera <jesusmaherrera@gmail.com>
2013-02-28 09:20:12 -0700

    Se incluyen archivos par ainiciar servidor

---
+++ iniciarservidor.cmd
@@ -0,0 +1,15 @@
+echo off
+color 30
+echo 			==================================
+echo 			=                                =
+echo 			=     SERVIDOR DE PAGINA WEB     =
+echo 			=                                =
+echo 			=   [ POR FAVOR NO CERRAR!!!! ]  =
+echo 			=                                =
+echo 			==================================
+echo.
+echo.
+
+
+cd C:\Users\Admin\Documents\GitHub\buro-de-credito
+python manage.py runserver 192.168.1.208:8000
