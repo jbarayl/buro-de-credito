9f6f4a8109aef7219c4392518a5bbc6370cd5ac3
Jesús Herrera <jesusmaherrera@gmail.com>
2014-07-02 11:25:31 -0600

    update settings

--- buro-de-credito/wsgi.py
+++ buro-de-credito/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buro-de-credito.settings.settings_prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buro-de-credito.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
